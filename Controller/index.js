ed714c4263455f68995acc2eddcadbdd72ec8c2f
Imran5123 <zohaibdon41@gmail.com>
2024-07-28 15:48:38 +0500

    crud-operation-in-nodeJs

---
+++ Controller/index.js
@@ -0,0 +1,7 @@
+const TeacherController=require("./teacher.controller");
+const GradeController=require("./grade.controller")
+
+module.exports = {
+    TeacherController,
+    GradeController
+  };
---
+++ database/model/grade.model.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (db) => {
+  const GRADE = db.define("grades", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    semester: {
+      type: DataTypes.TEXT,
+    },
+    course: {
+      type: DataTypes.TEXT,
+    },
+    grade: {
+      type: DataTypes.TEXT,
+    },
+  });
+
+  return GRADE;
+};
